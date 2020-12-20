ded7a6822db923219587098ea83398e1a5eaaf47
stylemate <orum93@naver.com>
2020-12-20 11:52:34 +0900

    neumorphism test

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var granimInstance = new Granim({
+var bkgGradient = new Granim({
   element: "#bkg-gradient",
   direction: "left-right",
   isPausedWhenNotInView: false,
@@ -17,3 +17,23 @@ var granimInstance = new Granim({
     }
   }
 });
+
+/*var boxGradient = new Granim({
+  element: "#center-text-canvas",
+  direction: "left-right",
+  isPausedWhenNotInView: false,
+  states: {
+    "default-state": {
+      //turquoise flow, atlas, pre-dawn, shore
+      gradients: [
+        ["#136A8A", "#267871"],
+        [
+          { color: "#FEAC5E" },
+          { color: "#C779D0" }
+        ],
+        ["#FFA17F", "#00223E"],
+        ["#70E1F5", "#FFD194"]
+      ]
+    }
+  }
+});*/
